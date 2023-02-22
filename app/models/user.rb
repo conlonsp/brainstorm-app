b9a6dd20fb810fe5b25abeb5bf4789a832e2a947
samuelconlon <conlonsp@yahoo.com>
2023-02-22 18:45:28 -0500

    added validations and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :bio, presence: true
+  validates :bio, length: { maximum: 250 }
+  validates :password, presence: true
+  validates :password, length: { 6..20 }
+  validates :avatar_url, presence: true
 end
