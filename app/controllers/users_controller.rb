e34b5ef087fe00bed49dec7b775e90b091757d56
samuelconlon <conlonsp@yahoo.com>
2023-02-22 18:52:16 -0500

    added create, show and error handling actions in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  def show
+    render json: User.find(session[:user_id]), status: :ok
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+
+  def user_invalid(e)
+    render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def user_not_found
+    render json: { errors: "User not found" }, status: :unauthorized
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :ideas
+  has_many :comments
+
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :bio, presence: true
