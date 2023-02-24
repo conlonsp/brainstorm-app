f8e6ffa0adaba9f87e4615efc89fb260c6605621
samuelconlon <conlonsp@yahoo.com>
2023-02-23 21:41:56 -0500

    commented associations back in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  # has_many :ideas
-  # has_many :comments
+  has_many :ideas
+  has_many :comments
 
   # validates :username, presence: true
   # validates :username, uniqueness: true
