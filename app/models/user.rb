90b4d90e9046f9349038675b1a1d5689b3d95e52
samuelconlon <conlonsp@yahoo.com>
2023-02-23 21:39:49 -0500

    commented out validations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :ideas
-  has_many :comments
+  # has_many :ideas
+  # has_many :comments
 
-  validates :username, presence: true
-  validates :username, uniqueness: true
-  validates :bio, presence: true
-  validates :bio, length: { maximum: 250 }
-  validates :password, presence: true
-  validates :password, length: { 6..20 }
-  validates :avatar_url, presence: true
+  # validates :username, presence: true
+  # validates :username, uniqueness: true
+  # validates :bio, presence: true
+  # validates :bio, length: { maximum: 250 }
+  # validates :password, presence: true
+  # validates :password, length: { maximum: 20 }
+  # validates :avatar_url, presence: true
 end
