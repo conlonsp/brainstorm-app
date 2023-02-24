0659dbcdab65a57c21b2dc826a09a896eb5f3219
samuelconlon <conlonsp@yahoo.com>
2023-02-24 12:57:28 -0500

    added handleSubmit func in Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState([])
+
+  function handleSubmit(e) {
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username, password})
+    }).then(r => {
+      if(r.ok) {
+        r.json().then(user => setUser(user))
+      } else {
+        r.json().then(err => setErrors(err))
+      }
+    })
+  }
+
   return (
     <h1>Login</h1>
   )
