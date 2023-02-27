329dcf81c4d3646eea14d56267fe94a2e17b0345
samuelconlon <conlonsp@yahoo.com>
2023-02-27 16:52:48 -0500

    passed setUser setter function from App to Home and from Home to Login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,7 +22,7 @@ function App() {
     <div>
       <NavBar user={user} setUser={setUser} />
       <Routes>
-        <Route path='/' element={<Home user={user}/>}/>
+        <Route path='/' element={<Home user={user} setUser={setUser} />}/>
         <Route path='/signup' element={<Signup
           setUser={setUser}
         />}/>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Login from './Login';
 
-function Home({ user }) {
+function Home({ user, setUser }) {
   return (
     <div>
       {!user ? 
         <div>
           <h1>Please Login or Signup</h1>
-          <Login />
+          <Login setUser={setUser} />
           <NavLink to='/signup'>Signup</NavLink>
         </div>
         :
