e17d3ee577a224ee2b12208fb7ce07a7b5a93d3f
samuelconlon <conlonsp@yahoo.com>
2023-02-27 15:11:15 -0500

    commented out User seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(
-  username: 'samc',
-  password: '123456',
-  password_confirmation: '123456',
-  bio: 'Hi, my name is Sam.',
-  avatar_url: 'https://www.google.com/search?q=random+image&oq=random&aqs=chrome.1.69i57j69i59j0i131i433i512j0i433i512l2j0i131i433i512l2j69i61.2225j0j7&sourceid=chrome&ie=UTF-8#imgrc=jsbCYSW7o-fL-M',
-)
+# user = User.create(
+#   username: 'samc',
+#   password: '123456',
+#   password_confirmation: '123456',
+#   bio: 'Hi, my name is Sam.',
+#   avatar_url: 'https://www.google.com/search?q=random+image&oq=random&aqs=chrome.1.69i57j69i59j0i131i433i512j0i433i512l2j0i131i433i512l2j69i61.2225j0j7&sourceid=chrome&ie=UTF-8#imgrc=jsbCYSW7o-fL-M',
+# )
