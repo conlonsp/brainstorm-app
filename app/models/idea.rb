ef309ea30f998b70f89d44636ba4556afa9beaec
samuelconlon <conlonsp@yahoo.com>
2023-03-03 19:35:55 -0500

    added validations to idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,8 @@
 class Idea < ApplicationRecord
   belongs_to :user
   has_many :comments
+  
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
