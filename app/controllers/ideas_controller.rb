86e17235d4c2ac679305b539111e16d3233e50ef
samuelconlon <conlonsp@yahoo.com>
2023-03-03 19:42:04 -0500

    added authorize action to ideas_controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,14 @@
 class IdeasController < ApplicationController
 
+  before_action :authorize
+
   def index
     render json: Idea.all, status: :ok
+  end
+
+  private
+
+  def authorize
+    render json: { errors: ["Not Authorized to View Content.  Please Log In to Continue"] }, status: :unauthorized unless session.include? :user_id
   end
 end
