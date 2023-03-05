868dd211d8e7f403832df6ac1d05a12e7cf5bf96
samuelconlon <conlonsp@yahoo.com>
2023-03-05 18:31:30 -0500

    imported useState and created title and content state

--- client/src/pages/NewIdea.js
+++ client/src/pages/NewIdea.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function newIdea() {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
 
   return (
     <form>
