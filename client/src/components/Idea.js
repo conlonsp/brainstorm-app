cb4161cc8eb96bc8025fd60b3f409f8392790ff5
samuelconlon <conlonsp@yahoo.com>
2023-03-05 11:02:12 -0500

    rendered associated username associated with idea

--- client/src/components/Idea.js
+++ client/src/components/Idea.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Idea({ idea }) {
 
+  console.log(idea.user.username)
+
   return (
     <div>
       <h1>{idea.title}</h1>
+      <h1>By: {idea.user.username}</h1>
       <p>{idea.content}</p>
       <h3>{idea.likes}</h3>
     </div>
