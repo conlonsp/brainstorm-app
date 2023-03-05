1fe44522f1f4fa6f9dc2157bbce58c8582e3e7e5
samuelconlon <conlonsp@yahoo.com>
2023-03-05 11:16:19 -0500

    created increment_like action for idea likes

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -11,4 +11,13 @@ class IdeasController < ApplicationController
   def authorize
     render json: { errors: ["Not Authorized to View Content."] }, status: :unauthorized unless session.include? :user_id
   end
+
+  def increment_likes
+    idea = idea.find_by(id: params[:id])
+    if idea
+      idea.update(idea.likes + 1)
+      render json: idea
+    end
+  end
+  
 end
