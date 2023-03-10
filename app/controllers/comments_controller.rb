d369b1f32a6efe2fa501eb2515fe205e1cafcf61
samuelconlon <conlonsp@yahoo.com>
2023-03-10 10:01:09 -0500

    added error rescue action in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
   def index
     if params[:idea_id]
       idea = Idea.find(params[:idea_id])
@@ -19,6 +21,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.permit(:content, :user_id, :idea_id)
+  end
+
+  def render_unprocessable_entity(e)
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
   
 end
