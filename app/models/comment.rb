ebadb6628608fde5f204b45188cda87ba14cccf4
samuelconlon <conlonsp@yahoo.com>
2023-03-10 13:53:58 -0500

    added validations for comment attributes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :idea
 
-  
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :idea_id, presence: true
 end
