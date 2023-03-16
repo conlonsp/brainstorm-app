b112ebd4e2a20a9d691bb017fb506e8a28483564
samuelconlon <conlonsp@yahoo.com>
2023-03-15 20:10:36 -0400

    updated idea update form and created handleChange function

--- client/src/components/UpdateIdeaForm.js
+++ client/src/components/UpdateIdeaForm.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function UpdateIdeaForm({ idea }) {
 
-  const {title, content, likes, user_id} = idea
+  const {title, content, likes, user_id: userId} = idea
+
+  const [updatedIdea, setUpdatedIdea] = useState({
+    title: title,
+    content: content,
+    likes: likes,
+    user_id: userId
+  })
 
   let navigate = useNavigate()
 
-  
+  function handleChange(e) {
+    setUpdatedIdea({
+      ...updatedIdea,
+      [e.target.name]: e.target.value
+    })
+  }
 
   return (
     <div>
@@ -16,16 +28,27 @@ function UpdateIdeaForm({ idea }) {
         <label htmlFor='title'>Title: </label>
         <input
           type='text'
+          name='title'
+          value={updatedIdea.title}
+          onChange={handleChange}
         />
         <label htmlFor='content'>Content: </label>
         <textarea
           type='text'
+          rows='4'
+          name='content'
+          value={updatedIdea.content}
+          onChange={handleChange}
         />
         <input
-
+          type='hidden'
+          name='likes'
+          value={updatedIdea.likes}
         />
         <input
-        
+          type='hidden'
+          name='user_id'
+          value={updatedIdea.user_id}
         />
       </form>
       <button onClick={() => navigate('/ideaboard')}>Back to Idea Board</button>
